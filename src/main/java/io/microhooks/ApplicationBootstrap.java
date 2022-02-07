6fb84f5b5a2fd8e6d644515450d8c6996d787b7c
Anass Zouine <a.zouine@aui.ma>
2022-02-08 00:02:07 +0100

    @Source Working Automaticall

--- src/main/java/io/microhooks/ApplicationBootstrap.java
+++ src/main/java/io/microhooks/ApplicationBootstrap.java
@@ -3,8 +3,11 @@ package io.microhooks;
 import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
 
+import io.microhooks.ddd.Source;
 import io.microhooks.ddd.internal.CustomListener;
 import io.microhooks.ddd.internal.SourceListener;
+
+import java.util.Iterator;
 
 import javax.persistence.EntityListeners;
 
@@ -13,13 +16,41 @@ import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.description.annotation.AnnotationDescription;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 
+import org.atteo.classindex.ClassIndex;
+
 public class ApplicationBootstrap implements ApplicationListener<ApplicationPreparedEvent> {
 
         public void onApplicationEvent(ApplicationPreparedEvent ev) {
                 System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
                 
                 ByteBuddyAgent.install();
+
+                Iterable<Class<?>> sourceClasses = ClassIndex.getAnnotated(Source.class);
+
+               
+
                 // lookup entity classes with @source
+                ByteBuddy byteBuddy = new ByteBuddy();
+                Iterator<Class<?>> iterator = sourceClasses.iterator();
+
+                if(iterator == null)
+                System.out.println("Iterator is null");
+
+                while(iterator.hasNext())
+                {
+                        Class <?> klass = iterator.next();
+                        System.out.println(klass.toString());
+                        
+                        byteBuddy
+                                .redefine(klass)
+                                .annotateType(AnnotationDescription.Builder.ofType(EntityListeners.class)
+                                                .defineTypeArray("value", SourceListener.class, CustomListener.class)
+                                                .build())
+                                .make()
+                                .load(klass.getClassLoader(),
+                                                ClassReloadingStrategy.fromInstalledAgent());
+                }
+                /*
                 new ByteBuddy()
                                 .redefine(TestEntity.class)
                                 .annotateType(AnnotationDescription.Builder.ofType(EntityListeners.class)
@@ -27,6 +58,6 @@ public class ApplicationBootstrap implements ApplicationListener<ApplicationPrep
                                                 .build())
                                 .make()
                                 .load(TestEntity.class.getClassLoader(),
-                                                ClassReloadingStrategy.fromInstalledAgent());
+                                                ClassReloadingStrategy.fromInstalledAgent());*/
         }
 }
--- src/main/java/io/microhooks/ddd/Source.java
+++ src/main/java/io/microhooks/ddd/Source.java
@@ -4,7 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.atteo.classindex.IndexAnnotated;
 
+@IndexAnnotated //class indexing
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Source {
