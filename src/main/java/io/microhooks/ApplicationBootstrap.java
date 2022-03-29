02564d38cd5e128e589f3b842841a151970b975f
Microhooks Machine <m.elkadirielhassani@aui.ma>
2022-03-29 01:21:48 +0000

    before branching to publish

--- src/main/java/io/microhooks/ApplicationBootstrap.java
+++ src/main/java/io/microhooks/ApplicationBootstrap.java
@@ -21,32 +21,32 @@ import org.atteo.classindex.ClassIndex;
 public class ApplicationBootstrap implements ApplicationListener<ApplicationPreparedEvent> {
 
         public void onApplicationEvent(ApplicationPreparedEvent ev) {
-                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
+               System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
 
-                ByteBuddyAgent.install();
+               ByteBuddyAgent.install();
 
-                Iterable<Class<?>> sourceClasses = ClassIndex.getAnnotated(Source.class);
+               Iterable<Class<?>> sourceClasses = ClassIndex.getAnnotated(Source.class);
 
-                // lookup entity classes with @source
-                ByteBuddy byteBuddy = new ByteBuddy();
-                Iterator<Class<?>> iterator = sourceClasses.iterator();
+               // lookup entity classes with @source
+               ByteBuddy byteBuddy = new ByteBuddy();
+               Iterator<Class<?>> iterator = sourceClasses.iterator();
 
-                if (iterator == null)
-                        System.out.println("Iterator is null");
+               if (iterator == null)
+                       System.out.println("Iterator is null");
 
-                while (iterator.hasNext()) {
-                        Class<?> klass = iterator.next();
-                        System.out.println(klass.toString());
+               while (iterator.hasNext()) {
+                       Class<?> klass = iterator.next();
+                       System.out.println(klass.toString());
 
-                        byteBuddy
-                                        .redefine(klass)
-                                        .annotateType(AnnotationDescription.Builder.ofType(EntityListeners.class)
-                                                        .defineTypeArray("value", SourceListener.class,
-                                                                        CustomListener.class)
-                                                        .build())
-                                        .make()
-                                        .load(klass.getClassLoader(),
-                                                        ClassReloadingStrategy.fromInstalledAgent());
-                }
+                       byteBuddy
+                                       .redefine(klass)
+                                       .annotateType(AnnotationDescription.Builder.ofType(EntityListeners.class)
+                                                       .defineTypeArray("value", SourceListener.class,
+                                                                       CustomListener.class)
+                                                       .build())
+                                       .make()
+                                       .load(klass.getClassLoader(),
+                                                       ClassReloadingStrategy.fromInstalledAgent());
+               }
         }
 }
